0067ce13d50b3ecc2d5fadd8c903e6dd115d0599
Antti Jaakkola <antti.jaakkola@remod.fi>
2016-03-01 21:09:17 +0200

    cachefile search

---
+++ src/ngflush/cachefiles.py
@@ -0,0 +1,46 @@
+import os
+import logging
+
+logger = logging.getLogger("ngflush")
+
+
+class InvalidCacheFile(Exception):
+    pass
+
+
+def get_key_from_file(file, path):
+    """
+    Get KEY: from cachefile
+
+    KEY: is located in position 145, so value starts from 145 + 5 = 150
+    Key value ends with newline.
+    :param path: path to file
+    :return: KEY from file
+    """
+    # Check file magic header
+    file.seek(0)
+    magic = file.read(8)
+    if magic != b"\x03\x00\x00\x00\x00\x00\x00\x00":
+        raise InvalidCacheFile("Invalid cache file %s" % path)
+    file.seek(150)
+    return file.readline().decode("utf-8").rstrip('\n')
+
+
+def find_cachefiles(directory, pattern):
+    """
+    Recursively find nginx cachefiles matching given pattern
+    :param directory: Base directory for search
+    :param pattern: Pattern to search
+    :return:
+    """
+    paths = []
+    for path, dirs, files in os.walk(directory):
+        for f in files:
+            file_path = os.path.join(path, f)
+            try:
+                with open(file_path, 'rb') as f:
+                    if pattern in get_key_from_file(f, file_path):
+                        paths.append(file_path)
+            except InvalidCacheFile as e:
+                logger.error(str(e))
+    return paths
--- src/tests/testcases.py
+++ src/tests/testcases.py
@@ -1,7 +1,12 @@
 import hashlib
+import random
+import string
+import tempfile
 from unittest import TestCase, main
+import io
 from ngflush.configuration import Config
 from ngflush.server import get_path, get_cache_key, get_key_hash
+from ngflush.cachefiles import *
 
 
 class TestGetPath(TestCase):
@@ -24,6 +29,10 @@ class TestGetPath(TestCase):
         self.assertTrue(get_path("5od93kid3") == Config.cache_path + "/id3/3k/5od93kid3",
                         "Get path returns invalid path for level 3:2")
 
+    def test_get_path_3_2_3(self):
+        Config.cache_levels = "3:2:3"
+        self.assertTrue(get_path("5od93kid3") == Config.cache_path + "/id3/3k/od9/5od93kid3",
+                        "Get path returns invalid path for level 3:2:3")
 
 class TestGetCacheKey(TestCase):
 
@@ -31,17 +40,17 @@ class TestGetCacheKey(TestCase):
         Config.get_parameter = "testflush"
 
     def test_get_cache_key_single_argument(self):
-        self.assertEquals(get_cache_key("foo?testflush=true"), "foo",
-                          "Get cache key failed to parser url without arguments")
+        self.assertEqual(get_cache_key("foo?testflush=true"), "foo",
+                                       "Get cache key failed to parser url without arguments")
 
     def test_get_cache_key_last_argument(self):
-        self.assertEquals(get_cache_key("foo?asdf=foo&testflush=true"), "foo?asdf=foo",
-                          "Get cache key failed to parse url with multiple arguments")
+        self.assertEqual(get_cache_key("foo?asdf=foo&testflush=true"), "foo?asdf=foo",
+                                       "Get cache key failed to parse url with multiple arguments")
 
     def test_get_cache_with_path(self):
-        self.assertEquals(get_cache_key("/asdf/foo?asdf=foo&ngflush=true&testflush=true"),
-                          "/asdf/foo?asdf=foo&ngflush=true",
-                          "Get cache key failed to parse url with path")
+        self.assertEqual(get_cache_key("/asdf/foo?asdf=foo&ngflush=true&testflush=true"),
+                                       "/asdf/foo?asdf=foo&ngflush=true",
+                                       "Get cache key failed to parse url with path")
 
 class TestKeyHashing(TestCase):
 
@@ -50,6 +59,73 @@ class TestKeyHashing(TestCase):
                          hashlib.md5(b"123asdfxd4d").hexdigest(),
                          "Hashlib don't return correct md5 value")
 
+
+def create_cachefile(filehandle, prefix):
+
+    path = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(random.randint(4, 10)))
+    key = "KEY: %s/%s\n" % (prefix.rstrip('/'), path)
+
+    filehandle.write(b"\x03")
+    for i in range(144):
+        filehandle.write(b"\x00")
+    filehandle.write(key.encode("utf-8"))
+    filehandle.flush()
+    filehandle.seek(0)
+
+
+class TestCacheFileParsing(TestCase):
+
+    def setUp(self):
+        self.cachefile = io.BytesIO()
+        self.cachefile.write(b"\x03" + b"\x00" * 144)
+        self.cachefile.write(b"KEY: httptesting.example.com/testing\n")
+        self.cachefile.seek(0)
+
+        self.broken_cachefile = io.BytesIO(b"\x00" * 145)
+
+    def test_parsing_invalid_file(self):
+        with self.assertRaises(InvalidCacheFile):
+            get_key_from_file(self.broken_cachefile, "testing")
+
+    def test_parsing_valid_file(self):
+        self.assertEqual(get_key_from_file(self.cachefile, "testing"),
+                         "httptesting.example.com/testing",
+                         "Invalid key returned from get_key_from_file")
+
+
+class TestCacheFileFind(TestCase):
+    def setUp(self):
+        self.base_directory = tempfile.mkdtemp()
+        self.subdirs = []
+        self.files = []
+
+        for i in range(5):
+            self.subdirs.append(tempfile.mkdtemp(dir=self.base_directory))
+
+        for dir in self.subdirs:
+            for i in range(5):
+                f, path = tempfile.mkstemp(dir=dir, text=False)
+                fd = os.fdopen(f, 'r+b')
+                create_cachefile(fd, "httptesting.example.com/")
+                self.files.append((fd, path))
+
+    def tearDown(self):
+        for file in self.files:
+            file[0].close()
+            os.remove(file[1])
+
+        for dir in self.subdirs:
+            os.rmdir(dir)
+
+        os.rmdir(self.base_directory)
+
+    def test_find_cachefiles(self):
+        files = find_cachefiles(self.base_directory, 'testing.example.com')
+        self.assertEqual(len(files), 25, "Find didn't find all files")
+
+    def test_find_cachefiles_wrong_pattern(self):
+        files = find_cachefiles(self.base_directory, 'testingg.example.com')
+        self.assertEqual(len(files), 0, "Find shouldn't find files")
 
 if __name__ == '__main__':
     main()
